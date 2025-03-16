e54696ea8896f62e8727849647bad52802b54ce5
CodeRed <85915113+CodeRed57@users.noreply.github.com>
2025-03-16 21:39:11 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,23 +5,31 @@ QB.Spawns = {
         coords = vector4(195.17, -933.77, 29.7, 144.5),
         location = 'legion',
         label = 'Legion Square',
+        description = "lorem ipsum dolor sit amet",
+        image = 'images/default.png',
     },
 
     policedp = {
         coords = vector4(428.23, -984.28, 29.76, 3.5),
         location = 'policedp',
         label = 'Police Department',
+        description = "lorem ipsum dolor sit amet",
+        image = 'images/default.png',
     },
 
     paleto = {
         coords = vector4(80.35, 6424.12, 31.67, 45.5),
         location = 'paleto',
         label = 'Paleto Bay',
+        description = "lorem ipsum dolor sit amet",
+        image = 'images/default.png',
     },
 
     motel = {
         coords = vector4(327.56, -205.08, 53.08, 163.5),
         location = 'motel',
         label = 'Motels',
+        description = "lorem ipsum dolor sit amet",
+        image = 'images/default.png',
     },
 }
