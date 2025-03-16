aa98225965f633c97b9e81ab86daea974bf538b1
CodeRed <85915113+CodeRed57@users.noreply.github.com>
2025-03-16 21:39:41 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -2,7 +2,10 @@ local Translations = {
     ui = {
         last_location = "Last Location",
         confirm = "Confirm",
+        select = "Select",
+        spawn_header = "Spawn Selector",
         where_would_you_like_to_start = "Where would you like to start?",
+        defaultDescription = 'a nice place to start',
     }
 }
 
